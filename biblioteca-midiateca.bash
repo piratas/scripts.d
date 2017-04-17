3d010c601313c132105c3bb321d20dd35d5e169b
r00t <root@partidopirata.org>
2017-04-17 09:08:18 +0200

    Envio automático de arquivos da biblioteca

---
+++ biblioteca-midiateca.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+## Adiciona arquivos da biblioteca pirata na midiateca pirata
+## Rodando na crontab do usuário mediagoblin@entwickler.partidopirata.org de hora em hora
+
+BIBLIOTECA_DIR="/var/lib/mediagoblin/mediagoblin/user_dev/media/public/media_entries"
+MIDIATECA_DIR="/var/lib/mediagoblin/git/midiateca"
+
+pushd ${MIDIATECA_DIR}
+rsync -avhP ${BIBLIOTECA_DIR}/ biblioteca/
+git add biblioteca
+git commit -am "Atualizando arquivos da biblioteca"
+git push origin master
+popd
+
